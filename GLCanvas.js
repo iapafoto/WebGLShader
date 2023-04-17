0c3786100876aad12c869bf097ace77ed1a54ff6
Sébastien Durand <33156785+iapafoto@users.noreply.github.com>
2023-04-17 12:31:24 +0200

    Update GLCanvas.js

--- GLCanvas.js
+++ GLCanvas.js
@@ -174,8 +174,6 @@ class GLCanvas {
 	}
 }
 
-
-
 // ------------------------------------------------------------------------------
 // Initialize a texture and load an image.
 // When the image finished loading copy it into the texture.
@@ -207,6 +205,19 @@ function loadTexture(canvas, url) {
 
 function isPowerOf2(value) {
   return (value & (value - 1)) === 0;
+}
+
+// ------------------------------------------------------------------------------
+// Initialize a texture from the rendering of a canvas
+function loadTexture(canvas) {
+	const gl = canvas.getContext("webgl2");
+	const texture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	return texture;
 }
 
 // ------------------------------------------------------------------------------------
